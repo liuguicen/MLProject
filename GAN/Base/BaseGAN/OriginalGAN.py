3688e5838ec1bcba07c8d7dc216897b0dcc69a40
liuguicen <2583657917@qq.com>
2021-07-31 00:14:58 +0800

    参考网上，修改原始GAN代码，生成的图片多样性和逼真度上升一个等级，主要增加通过生成的容量：bn+更深的网络

--- GAN/Base/BaseGAN/OriginalGAN.py
+++ GAN/Base/BaseGAN/OriginalGAN.py
@@ -16,21 +16,29 @@ from ml_base.run_record import RunRecord
 import os
 from os import path
 
+from common_dataset import Minist
+from torch.utils.data import DataLoader
+from torch.optim import Adam
+from torchvision import transforms
+
 
 class Config:
     checkPointCount = 20
     batch_size = 256
     epoch = 20
+    '''
+    输入生成器的尺寸
+    '''
+    # input_w = 10
+    input_w = 10
     # 生成器每一层尺寸
-    layers = [4 * 4, 7 * 7, 14 * 14, 28 * 28]
+    # layers = [4 * 4, 7 * 7, 14 * 14, 28 * 28]
+    layers = [input_w * input_w, 128, 256, 512, 1024, 784]
     '''
     判别器每一层的深度
     '''
     depth = [8, 16, 32]
-    '''
-    输入生成器的尺寸
-    '''
-    input_w = 4
+
 
 
 from CommonModels import NormalConvLayer
@@ -39,8 +47,52 @@ from CommonModels import NormalConvLayer
 class Conv3x3(nn.Sequential):
     def __init__(self, in_channel, out_channel):
         nn.Module.__init__(self)
-        super().add_module('conv', nn.Conv2d(in_channel, out_channel, kernel_size=3, padding=1))
+        super().add_module('conv', nn.Conv2d(in_channel, out_channel, kernel_size=3, stride=2, padding=1))
+        super().add_module("bn", nn.BatchNorm2d(out_channel))
         super().add_module('relu', nn.LeakyReLU())
+
+
+class Generator(nn.Module):
+    def __init__(self):
+        super().__init__()
+        self.seq = nn.Sequential()
+        for i, w in enumerate(Config.layers):
+            if i < len(Config.layers) - 1:
+                self.seq.add_module('linear %d - %d' % (Config.layers[i], Config.layers[i + 1]),
+                                    nn.Linear(Config.layers[i], Config.layers[i + 1]))
+                if i != 0:
+                    self.seq.add_module("bn %d" % (i + 1), nn.BatchNorm1d(Config.layers[i + 1], 0.8))
+                if i < len(Config.layers) - 2:
+                    self.seq.add_module('relu%d' % i, nn.LeakyReLU())
+                else:
+                    self.seq.add_module('last tanh', nn.Tanh())
+                    # 相对于singmod值区间是0-1，tanh和sigmod是线性关系，但是它直接将区间变到-1 - 1
+
+    def forward(self, x):
+        x = self.seq(x)
+        x = x.view(x.size()[0], 1, 28, 28)
+        # x = x * 2 - 1
+        return x
+
+
+# class Discriminator(nn.Module):
+#     def __init__(self):
+#         super(Discriminator, self).__init__()
+#
+#         self.model = nn.Sequential(
+#             nn.Linear(28 * 28, 512),
+#             nn.LeakyReLU(0.2, inplace=True),
+#             nn.Linear(512, 256),
+#             nn.LeakyReLU(0.2, inplace=True),
+#             nn.Linear(256, 1),
+#             nn.Sigmoid(),
+#         )
+#
+#     def forward(self, img):
+#         img_flat = img.view(img.size(0), -1)
+#         validity = self.model(img_flat)
+#
+#         return validity
 
 
 class Discriminator(nn.Module):
@@ -54,11 +106,11 @@ class Discriminator(nn.Module):
             else:
                 self.conv_seq.add_module('conv layer %d - %d' % (dep // 2, dep),
                                          Conv3x3(dep // 2, dep))
-            if i < len(Config.depth) - 1:
-                self.conv_seq.add_module('pool %d' % i, nn.MaxPool2d(2))
+            # if i < len(Config.depth) - 1:
+            #     self.conv_seq.add_module('pool %d' % i, nn.MaxPool2d(2))
         # 输入尺寸是28 * 28，
-        self.linear = nn.Linear(7 * 7 * Config.depth[-1], 7 * 7 * Config.depth[-1] // 4)
-        self.out_lin = nn.Linear(7 * 7 * Config.depth[-1] // 4, 1)
+        self.linear = nn.Linear(4 * 4 * Config.depth[-1], 4 * 4 * Config.depth[-1] // 4)
+        self.out_lin = nn.Linear(4 * 4 * Config.depth[-1] // 4, 1)
 
     def forward(self, x: torch.Tensor):
         '''
@@ -73,26 +125,6 @@ class Discriminator(nn.Module):
         return x
 
 
-class Generator(nn.Module):
-    def __init__(self):
-        super().__init__()
-        self.seq = nn.Sequential()
-        for i, w in enumerate(Config.layers):
-            if i < len(Config.layers) - 1:
-                self.seq.add_module('linear %d - %d' % (Config.layers[i], Config.layers[i + 1]),
-                                    nn.Linear(Config.layers[i], Config.layers[i + 1]))
-                if i < len(Config.layers) - 2:
-                    self.seq.add_module('relu%d' % i, nn.LeakyReLU())
-                else:
-                    self.seq.add_module('last sigmoid', nn.Sigmoid())
-
-    def forward(self, x):
-        x = self.seq(x)
-        x = x.view(x.size()[0], 1, 28, 28)
-        x = x * 2 - 1
-        return x
-
-
 import ml_util
 
 D = Discriminator()
@@ -102,13 +134,9 @@ G = Generator()
 G.to(ml_util.gpu)
 # print(G)
 
-from common_dataset import Minist
-from torch.utils.data import DataLoader
-from torch.optim import Adam
-from torchvision import transforms
 
-dOpt = Adam(D.parameters())
-gOpt = Adam(G.parameters())
+dOpt = Adam(D.parameters(), lr=0.0002)
+gOpt = Adam(G.parameters(), lr=0.0002)
 lossFunction = nn.BCELoss()
 
 
@@ -118,6 +146,8 @@ def train():
     G_label = torch.ones([Config.batch_size]).unsqueeze(1).to(ml_util.gpu)
     D_label = torch.cat([torch.ones([Config.batch_size]), torch.zeros([Config.batch_size])], 0).unsqueeze(1).to(
         ml_util.gpu)
+
+    test_z = torch.randn([8, Config.input_w * Config.input_w]).to(ml_util.gpu)
     lossGList = []
     lossDList = []
     runRecord = RunRecord.readFromDisk()
@@ -125,56 +155,56 @@ def train():
         runRecord = RunRecord()
 
     d_step = 0
+    lossG = None
     for epoch in range(Config.epoch):
         print(f'start {epoch} epoch')
         for iter, item in enumerate(dataLoader):
             # 训练判别器k步
-            if d_step < 1:
-                d_step += 1
-                image = item[0].to(ml_util.gpu)
-                if image.size()[0] != Config.batch_size:  # 最后一批
-                    continue
-                p_data = D(image)
+            d_step += 1
+            image = item[0].to(ml_util.gpu)
+            if image.size()[0] != Config.batch_size:  # 最后一批
+                continue
+            p_data = D(image)
 
-                z = torch.randn([Config.batch_size, Config.input_w * Config.input_w]).to(ml_util.gpu)
-                g_image = G(z)  # type:torch.Tensor
-                g_image.detach()
-                p_g = D(g_image)
-                print('p_data =  ', p_data.mean().item(), ' p_g =  ', p_g.mean().item())
-                D_out = torch.cat([p_data, p_g], 0)
-                lossD = lossFunction(D_out, D_label)  # type:torch.Tensor
-                dOpt.zero_grad()
-                lossD.backward()
-                dOpt.step()
-                print('优化D一次')
-                print('p_data = ', D(image).mean().item(), 'p_g = ', D(g_image).mean().item())
+            z = torch.randn([Config.batch_size, Config.input_w * Config.input_w]).to(ml_util.gpu)
+            g_image = G(z)  # type:torch.Tensor
+            g_image.detach()
+            p_g = D(g_image)
+            D_out = torch.cat([p_data, p_g], 0)
+            lossD = lossFunction(D_out, D_label)  # type:torch.Tensor
+            dOpt.zero_grad()
+            D.zero_grad()
+            lossD.backward()
+            dOpt.step()
+
+            if d_step < 1:
                 continue
             d_step = 0
-
             print('优化D完成')
+            print('p_data = ', D(image).mean().item(), 'p_g = ', D(g_image).mean().item())
+            print('loss G = ', lossFunction(p_g, G_label).item(), 'loss D = ', lossD.item())
+
             # 训练生成器一步
-            lossG = None
             for i in range(1):  # 分类器能力太强，多跑几步生成器
                 z = torch.randn([Config.batch_size, Config.input_w * Config.input_w]).to(ml_util.gpu)
                 g_image = G(z)
                 p_g = D(g_image)
                 lossG = lossFunction(p_g, G_label)  # type:torch.Tensor
                 gOpt.zero_grad()
+                G.zero_grad()
                 lossG.backward()
                 gOpt.step()
-                print('优化G一次')
-                print('p_data = ', D(image).mean().item(), 'p_g = ', D(g_image).mean().item())
 
             print('优化G完成')
+            print('p_data = ', D(image).mean().item(), 'p_g = ', D(g_image).mean().item())
+            print('loss G = ', lossG.item(), 'loss D = ', lossD.item())
             lossGList.append(lossG.item())
             lossDList.append(lossD.item())
-            print('loss G = ', lossG.item(), 'loss D = ', lossD.item())
 
             if iter % Config.checkPointCount == 0:
-                torchvision.utils.save_image(image[0:8], f'{runRecord.tes_res_dir}/{epoch}_{iter}_real.jpg')
+                # torchvision.utils.save_image(image[0:8], f'{runRecord.tes_res_dir}/{epoch}_{iter}_real.jpg')
 
-                z = torch.randn([8, Config.input_w * Config.input_w]).to(ml_util.gpu)
-                testOut = G(z)
+                testOut = G(test_z)
                 torchvision.utils.save_image(testOut, f'{runRecord.tes_res_dir}/{epoch}_{iter}_generate.jpg')
                 runRecord.saveRunRecord(epoch, iter)
 
---
+++ GAN/Base/BaseGAN/OriginalGAN_learning.py
@@ -0,0 +1,174 @@
+import argparse
+import os
+import numpy as np
+import math
+
+import torchvision.transforms as transforms
+from torchvision.utils import save_image
+
+from torch.utils.data import DataLoader
+from torchvision import datasets
+from torch.autograd import Variable
+
+import torch.nn as nn
+import torch.nn.functional as F
+import torch
+
+os.makedirs("images", exist_ok=True)
+
+
+class Config:
+    pass
+
+
+config = Config()
+config.n_epochs = 200
+config.batch_size = 64
+config.lr = 0.0002
+config.b1 = 0.5
+config.b2 = 0.999
+config.n_cpu = 8
+config.latent_dim = 100
+config.img_size = 28
+config.channels = 1
+config.sample_interval = 400
+print(config)
+
+img_shape = (config.channels, config.img_size, config.img_size)
+
+cuda = True if torch.cuda.is_available() else False
+
+
+class Generator(nn.Module):
+    def __init__(self):
+        super(Generator, self).__init__()
+
+        def block(in_feat, out_feat, normalize=True):
+            layers = [nn.Linear(in_feat, out_feat)]
+            if normalize:
+                layers.append(nn.BatchNorm1d(out_feat, 0.8))
+            layers.append(nn.LeakyReLU(0.2, inplace=True))
+            return layers
+
+        self.model = nn.Sequential(
+            *block(config.latent_dim, 128, normalize=False),
+            *block(128, 256),
+            *block(256, 512),
+            *block(512, 1024),
+            nn.Linear(1024, int(np.prod(img_shape))),
+            nn.Tanh()
+        )
+
+    def forward(self, z):
+        img = self.model(z)
+        img = img.view(img.size(0), *img_shape)
+        return img
+
+
+class Discriminator(nn.Module):
+    def __init__(self):
+        super(Discriminator, self).__init__()
+
+        self.model = nn.Sequential(
+            nn.Linear(int(np.prod(img_shape)), 512),
+            nn.LeakyReLU(0.2, inplace=True),
+            nn.Linear(512, 256),
+            nn.LeakyReLU(0.2, inplace=True),
+            nn.Linear(256, 1),
+            nn.Sigmoid(),
+        )
+
+    def forward(self, img):
+        img_flat = img.view(img.size(0), -1)
+        validity = self.model(img_flat)
+
+        return validity
+
+
+# Loss function
+adversarial_loss = torch.nn.BCELoss()
+
+# Initialize generator and discriminator
+generator = Generator()
+discriminator = Discriminator()
+
+if cuda:
+    generator.cuda()
+    discriminator.cuda()
+    adversarial_loss.cuda()
+
+# opture data loader
+os.makedirs("../../data/mnist", exist_ok=True)
+dataloader = torch.utils.data.DataLoader(
+    datasets.MNIST(
+        "../../data/mnist",
+        train=True,
+        download=True,
+        transform=transforms.Compose(
+            [transforms.Resize(config.img_size), transforms.ToTensor(), transforms.Normalize([0.5], [0.5])]
+        ),
+    ),
+    batch_size=config.batch_size,
+    shuffle=True,
+)
+
+# Optimizers
+optimizer_G = torch.optim.Adam(generator.parameters(), lr=config.lr, betas=(config.b1, config.b2))
+optimizer_D = torch.optim.Adam(discriminator.parameters(), lr=config.lr, betas=(config.b1, config.b2))
+
+Tensor = torch.cuda.FloatTensor if cuda else torch.FloatTensor
+
+# ----------
+#  Training
+# ----------
+
+for epoch in range(config.n_epochs):
+    for i, (imgs, _) in enumerate(dataloader):
+
+        # Adversarial ground truths
+        valid = Variable(Tensor(imgs.size(0), 1).fill_(1.0), requires_grad=False)
+        fake = Variable(Tensor(imgs.size(0), 1).fill_(0.0), requires_grad=False)
+
+        # picture input
+        real_imgs = Variable(imgs.type(Tensor))
+
+        # -----------------
+        #  Train Generator
+        # -----------------
+
+        optimizer_G.zero_grad()
+
+        # Sample noise as generator input
+        z = Variable(Tensor(np.random.normal(0, 1, (imgs.shape[0], config.latent_dim))))
+
+        # Generate a batch of images
+        gen_imgs = generator(z)
+
+        # Loss measures generator's ability to fool the discriminator
+        g_loss = adversarial_loss(discriminator(gen_imgs), valid)
+
+        g_loss.backward()
+        optimizer_G.step()
+
+        # ---------------------
+        #  Train Discriminator
+        # ---------------------
+
+        optimizer_D.zero_grad()
+
+        # Measure discriminator's ability to classify real from generated samples
+        real_loss = adversarial_loss(discriminator(real_imgs), valid)
+        fake_loss = adversarial_loss(discriminator(gen_imgs.detach()), fake)
+        d_loss = (real_loss + fake_loss) / 2
+
+        d_loss.backward()
+        optimizer_D.step()
+
+        print(
+            "[Epoch %d/%d] [Batch %d/%d] [D loss: %f] [G loss: %f]"
+            % (epoch, config.n_epochs, i, len(dataloader), d_loss.item(), g_loss.item())
+        )
+
+        batches_done = epoch * len(dataloader) + i
+        if batches_done % config.sample_interval == 0:
+            save_image(gen_imgs.data[:25], "images/%d.png" % batches_done, nrow=5, normalize=True)
