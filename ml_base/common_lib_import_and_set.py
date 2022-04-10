a72dc6bcb365e56cccad6ddf8c631a07390e54cb
liuguicen <2583657917@qq.com>
2022-04-11 01:55:48 +0800

    优化

--- ml_base/common_lib_import_and_set.py
+++ ml_base/common_lib_import_and_set.py
@@ -4,18 +4,32 @@
 # 然后直接使用相关类或者文件即可
 
 # 用法 from ml_base.common_lib_import import *
+
+# python
+import os
 from os import path
-import FileUtil
 import random
-import image_util
+
+# 三方
 from matplotlib import pyplot as plt
 import numpy as np
+import torch
+from PIL import Image
+import cv2
 
+# 自己的
+import FileUtil
+import image_util
 # 设置plt支持中文
 plt.rcParams['font.sans-serif'] = ['simhei'] # simhei 这个字体需要先手动下载放到plt的目录下，参考网上方法
 plt.rcParams['axes.unicode_minus'] = False
 if __name__ == '__main__':
+    os
     FileUtil
     image_util
+    torch
+    Image
+    cv2
     random.random()
     path.join('a', 'b')
+    np.ndarray
